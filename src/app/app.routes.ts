2512dfd97f29aac2b023d71c44551ecafc69a1db
Daniel Develop <57878266+iath-dev@users.noreply.github.com>
2025-07-31 14:44:51 -0500

    feat: basic pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'markers',
+    loadComponent: () =>
+      import('@app/pages/markers-page/markers-page.component').then(
+        (m) => m.MarkersPageComponent
+      ),
+    title: 'Marcadores',
+  },
+  {
+    path: 'houses',
+    loadComponent: () =>
+      import('@app/pages/houses-page/houses-page.component').then(
+        (m) => m.HousesPageComponent
+      ),
+    title: 'Casas - Propiedades disponibles',
+  },
+  {
+    path: 'fullscreen',
+    loadComponent: () =>
+      import(
+        '@app/pages/fullscreen-map-page/fullscreen-map-page.component'
+      ).then((m) => m.FullscreenMapPageComponent),
+    title: 'FullScreen Map',
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen',
+  },
+];
---
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './fullscreen-map-page.component.html',
+})
+export class FullscreenMapPageComponent {}
